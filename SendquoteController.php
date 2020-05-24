524737a736f9c89124ad4c2fab95edcfb49a92ef
macdanniels <hoghidan1@gmail.com>
2020-05-24 20:43:21 +0100

    latest check
    form should check for duplicates and return error if exist and else execute insertion of form entry to database.

--- SendquoteController.php
+++ SendquoteController.php
@@ -2,23 +2,27 @@
 
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use DB;
+use  Illuminate\Support\facades\DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\Model;
+use Auth;
+
+use App\Http\Controllers\model;
+use data;
+use Illuminate\Support\MessageBag;
+
 
 
 class SendquoteController extends Controller
 {
  public function quotemessage(){
-
-    return view('Sendquote.quotemessage');
+ 
+  return view('Sendquote.quotemessage');
 
   }
 
- 
-   
     public function store(Request $request){
 
       // for checking if code works   
@@ -27,13 +31,21 @@ class SendquoteController extends Controller
      //validating data inputted by users.. 
         $this->validate($request, [
 
-             'fname' => 'required',
-              'lname' => 'required',
-               'pno' => 'required',
+               'fname' => 'required',
+               'lname' => 'required',
+               'pno' => 'required|numeric|min:14',
                'message' => 'required',
                'service' => 'required'
              
-            ]);  
+             ], [], 
+         
+          [
+              'fname' => 'First Name',
+              'lname' => 'Last Name',
+              'message' => 'Message',
+              'service' => 'service',
+              'pno' => 'Phone Number'
+              ]);
 
        // accepting data.................
            $fname = $request-> input('fname');
@@ -42,27 +54,30 @@ class SendquoteController extends Controller
            $pno = $request-> input('pno');
            $service = $request-> input('service');
 
-                                     
-
-        if(DB::table('sendquotes')->where('message', $message)){
-
-              return back()->with('errors', 'Request already exists');
-            }
-             
-               else{  
-
-        
-        $data= array('fname'=>$fname,
-                   'lname'=>$lname,
-                   'message'=>$message,
-                    'pno'=>$pno,
-                    'service'=>$service);
-
-        DB::table('sendquotes')->insert($data);
-          
-       return redirect()->back()->with('flash_message', 'Quote Received'); 
-        
-       }
        
 
-}
+          // this line of code should check for duplicates,and return error
+
+          if ($data = DB::table('sendquotes')->exists()){
+
+          return redirect()->back()->with('flash_message', 'You have Supplied Quote already!');  
+      
+           }
+          // this line of code should execute if no duplicate value, and store entry. 
+          else{ 
+              
+           $data= array('fname'=>$fname,
+           'lname'=>$lname,
+           'message'=>$message,
+           'pno'=>$pno,
+           'service'=>$service);
+           DB::table('sendquotes')->insert($data);
+        
+           return redirect()->back()->with('flash_message', 'Quote Received'); 
+  
+                  }
+
+                }
+  
+            }
+
